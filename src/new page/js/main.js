8b09e1b2d63c20fb093ff5eb7cbd48b775e7f4da
Andrei <andrei.cristea997@gmail.com>
2019-10-07 17:55:47 +0200

    Second carousel is ready for usage

--- src/new page/js/main.js
+++ src/new page/js/main.js
@@ -3,6 +3,12 @@ $('.carousel').flickity({
   wrapAround: true,
   groupCells: 2,
   cellAlign: "center",
+  arrowShape: {
+    x0: 10,
+    x1: 70, y1: 50,
+    x2: 70, y2: 50,
+    x3: 70
+  }
   
   // autoPlay: true,
   // autoPlay: 5000,
@@ -10,3 +16,68 @@ $('.carousel').flickity({
 
 });
 
+function myFunction() {
+  var x = document.getElementById('circle');
+  if (x.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    x.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    x.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
+
+function myFunctiontwo() {
+  var y = document.getElementById('circletwo');
+  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
+function myFunctionThree() {
+  var y = document.getElementById('circleThree');
+  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
+function myFunctionFour() {
+  var y = document.getElementById('circleFour');
+  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
+function myFunctionFive() {
+  var y = document.getElementById('circleFive');
+  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
+function myFunctionSix() {
+  var y = document.getElementById('circleSix');
+  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
+function myFunctionSeven() {
+  var y = document.getElementById('circleSeven');
+  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
+function myFunctionEight() {
+  var y = document.getElementById('circleEight');
+  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
+    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  } else {
+    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+};
