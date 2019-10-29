ecf8d1f71929db05be9ea574e28cf518fad6aead
Andrei <andrei.cristea997@gmail.com>
2019-10-29 19:50:29 +0100

    Finally change buttons to radio buttons. Now it is working properly.

--- src/new page/js/main.js
+++ src/new page/js/main.js
@@ -16,68 +16,8 @@ $('.carousel').flickity({
 
 });
 
-function myFunction() {
-  var x = document.getElementById('circle');
-  if (x.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    x.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    x.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
-
-function myFunctiontwo() {
-  var y = document.getElementById('circletwo');
-  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
-function myFunctionThree() {
-  var y = document.getElementById('circleThree');
-  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
-function myFunctionFour() {
-  var y = document.getElementById('circleFour');
-  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
-function myFunctionFive() {
-  var y = document.getElementById('circleFive');
-  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
-function myFunctionSix() {
-  var y = document.getElementById('circleSix');
-  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
-function myFunctionSeven() {
-  var y = document.getElementById('circleSeven');
-  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
-function myFunctionEight() {
-  var y = document.getElementById('circleEight');
-  if (y.style.backgroundColor === 'rgba(0, 0, 0, 0)') {
-    y.style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  } else {
-    y.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-  }
-};
+let radio_num = document.querySelectorAll("#radio");
+function change_radio() {
+  let form_control = document.querySelector(".form-control");
+  form_control.style.color = "red";
+}
