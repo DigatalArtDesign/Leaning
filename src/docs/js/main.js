13442f2e65128661d2d94bcb884d2f70f410045a
Andrei <andrei.cristea997@gmail.com>
2019-11-06 13:07:11 +0100

    Made Modal Window

--- src/docs/js/main.js
+++ src/docs/js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 $('.carousel').flickity({
   freeScroll: true,
   wrapAround: true,
@@ -16,33 +17,4 @@ $('.carousel').flickity({
 
 });
 
-let radio_num = document.querySelectorAll("#radio");
-function change_radio() {
-  let form_control = document.querySelector(".form-control");
-  form_control.style.color = "red";
-}
-// Get the modal
-let modal = document.querySelector('.login-modal');
-// Get the button which opens the modal
-let btn = document.querySelector('.login');
-// Get the span element which closes the modal
-let span = document.querySelector('.close');
-let container = document.querySelector('.container-fluid');
-btn.onclick = function () {
-  modal.style.display = 'block';
-  container.classList.remove('scaleAnimanitionOut');
-}
-span.onclick = function () {
-  container.classList.add('scaleAnimanitionOut');
-  setTimeout(() => {
-    modal.style.display = 'none';
-  }, 400);
-}
-window.onclick = function (event) {
-  if (event.target == modal) {
-    container.classList.add('scaleAnimanitionOut');
-    setTimeout(() => {
-      modal.style.display = 'none';
-    }, 400);
-  }
-}
+
